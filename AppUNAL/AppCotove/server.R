2b200005775a76ef52f2db41e8c2394d0e8a18aa
Edimer <edimerfrv@gmail.com>
2020-09-09 06:37:54 -0500

    app Unal shiny

---
+++ AppUNAL/AppCotove/server.R
@@ -0,0 +1,106 @@
+# ----- Bibliotecas -----
+library(shiny)
+library(tidyverse)
+library(rlang)
+library(DT)
+
+# ----- Datos -----
+load("../cotoveFinal.Rdata")
+datos4 <- datos4 %>% 
+  filter(edad_dest > 0) %>% 
+  filter(peso_dest > 0) %>% 
+  filter(peso_nto > 0) %>% 
+  filter(ganancia_dia > 0)
+
+shinyServer(function(input, output) {
+  
+  # Descriptivo ----
+  funDescrip <- reactive({
+    dataDescrip = datos4 %>% 
+      filter(fecha_nac >= input$fecha1[1] & fecha_nac <= input$fecha1[2]) %>% 
+      filter(raza %in% input$raza1)
+    
+    if(input$tipo1 == "General"){
+      dataDescrip %>% 
+        select(peso_nto, peso_dest, ganancia_dia, edad_dest) %>% 
+        gather(key = "Variable", value = "value") %>% 
+        group_by(Variable) 
+    } else if(input$tipo1 == "Sexo"){
+      dataDescrip %>% 
+        select(peso_nto, peso_dest, ganancia_dia, edad_dest, sexo) %>% 
+        gather(key = "Variable", value = "value", -sexo) %>% 
+        rename(Sexo = sexo) %>% 
+        group_by(Variable, Sexo)
+    } else if(input$tipo1 == "Raza") {
+      dataDescrip %>% 
+        select(peso_nto, peso_dest, ganancia_dia, edad_dest, raza) %>% 
+        gather(key = "Variable", value = "value", -raza) %>% 
+        rename(Raza = raza) %>% 
+        group_by(Variable, Raza)
+    } else {
+      dataDescrip %>% 
+        select(peso_nto, peso_dest, ganancia_dia, edad_dest, año_nac) %>% 
+        gather(key = "Variable", value = "value", -año_nac) %>% 
+        rename(Año = año_nac) %>% 
+        group_by(Variable, Año)
+    }
+  })
+  
+  output$tabla <- renderDataTable({
+    funDescrip() %>%  
+      summarise(Promedio = round(mean(value, na.rm = TRUE), digits = 2),
+                Mínimo = round(min(value, na.rm = TRUE), digits = 2),
+                Máximo = round(max(value, na.rm = TRUE), digits = 2),
+                DE = round(sd(value, na.rm = TRUE), digits = 2),
+                CV = round((DE/Promedio)*100, digits = 2),
+                N = n()) %>% 
+      mutate(Variable = gsub("edad_dest", "Edad destete (días)", Variable),
+             Variable = gsub("ganancia_dia", "Ganancia diaria (g)", Variable),
+             Variable = gsub("peso_dest", "Peso destete (kg)", Variable),
+             Variable = gsub("peso_nto", "Peso nacimiento (kg)", Variable)) %>% 
+      datatable(rownames = FALSE,
+                extensions = c("Buttons"),
+                options = list(
+                  dom = "Bfrtip",
+                  buttons = c("pdf", "excel"),
+                  language = list(url = '//cdn.datatables.net/plug-ins/1.10.11/i18n/Spanish.json')
+                ))
+  })
+  
+  # Gráfico -----
+  output$grafico <- renderPlotly({
+    if(input$tipo2 == "Boxplot"){
+      datos4 %>% 
+        filter(edad_dest > 0) %>% 
+        filter(peso_dest > 0) %>% 
+        filter(peso_nto > 0) %>% 
+        filter(ganancia_dia > 0) %>% 
+        mutate(año_nac = as.factor(año_nac)) %>% 
+        filter(raza %in% input$raza2) %>% 
+        ggplot(aes(x = año_nac, y = !!parse_quo(input$variable, env = global_env()))) +
+        geom_boxplot() +
+        labs(x = "Año") +
+        theme_light() +
+        theme(axis.text.x = element_text(angle = 35, hjust = 1))
+        
+    } else if(input$tipo2 == "Tendencias"){
+      datos4 %>% 
+        filter(edad_dest > 0) %>% 
+        filter(peso_dest > 0) %>% 
+        filter(peso_nto > 0) %>% 
+        filter(ganancia_dia > 0) %>% 
+        mutate(año_nac = as.factor(año_nac)) %>% 
+        filter(raza %in% input$raza2) %>% 
+        group_by(año_nac) %>% 
+        summarise(promedio = mean(!!parse_quo(input$variable, env = global_env()),
+                                  na.rm = TRUE)) %>% 
+        ggplot(aes(x = año_nac, y = promedio)) +
+        geom_point(size = 2) +
+        geom_line(group = 1) +
+        labs(x = "Año", y = "Promedio") +
+        theme_light() +
+        theme(axis.text.x = element_text(angle = 35, hjust = 1))
+    }
+  })
+  
+})
---
+++ AppUNAL/AppCotove/ui.R
@@ -0,0 +1,108 @@
+# ----- Bibliotecas -----
+library(shiny)
+library(shinythemes)
+library(DT)
+library(shinycssloaders)
+library(shinyWidgets)
+library(plotly)
+
+# ----- Datos -----
+load("../cotoveFinal.Rdata")
+
+# ----- Shiny UI ------
+shinyUI(fluidPage(
+  
+  theme = shinytheme("cosmo"),
+  
+  # ------- Descriptivo ---------
+  navbarPage(
+    "App Cotové",
+    tabPanel(
+      "Descriptivo",
+      sidebarLayout(
+        sidebarPanel(
+          dateRangeInput(
+            inputId = "fecha1",
+            label = "Fecha de nacimiento:",
+            start  = min(datos4$fecha_nac, na.rm = TRUE),
+            end = max(datos4$fecha_nac, na.rm = TRUE),
+            language = "es"
+          ),
+          pickerInput(
+            inputId = "raza1",
+            label = "Raza:",
+            choices = levels(datos4$raza),
+            selected = levels(datos4$raza),
+            multiple = TRUE,
+            options = list(
+              `actions-box` = TRUE,
+              `deselect-all-text` = "Ninguna",
+              `select-all-text` = "Todas",
+              `none-selected-text` = "Seleccione una o más razas",
+              size = 5
+            )
+          ),
+          radioButtons(
+            inputId = "tipo1",
+            label = "Resumen:",
+            choices = c("General", "Sexo", "Raza", "Año"),
+            selected = "General"
+          )
+        ),
+        mainPanel(
+          withSpinner(
+            dataTableOutput(outputId = "tabla"),
+            type = 8
+          )
+        )
+      )
+    ),
+    # ------- Distribuciones ---------
+    tabPanel(
+      "Variación Temporal",
+      fluidRow(
+        column(
+          width = 6,
+        ),
+        column(
+          width = 6
+        )
+      ),
+      sidebarLayout(
+        sidebarPanel(
+          selectInput(
+            inputId = "raza2",
+            label = "Raza:",
+            choices = levels(datos4$raza),
+            selected = "CEB/100",
+            multiple = FALSE
+          ),
+          prettyRadioButtons(
+            inputId = "variable",
+            label = "Variable:", 
+            choices = names(datos4 %>% select(peso_nto, peso_dest, ganancia_dia, edad_dest)),
+            icon = icon("check"), 
+            bigger = TRUE,
+            status = "primary"
+          )
+        ),
+        mainPanel(
+          radioGroupButtons(
+            inputId = "tipo2",
+            label = "",
+            choices = c("Boxplot", "Tendencias"),
+            status = "primary",
+            checkIcon = list(
+              yes = icon("ok", lib = "glyphicon"),
+              no = icon("remove", lib = "glyphicon")),
+            justified = TRUE
+          ),
+          withSpinner(
+            plotlyOutput(outputId = "grafico"),
+            type = 8
+          )
+        )
+      )
+    )
+  )
+))
